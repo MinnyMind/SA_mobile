a85b85d4228db36ec645431b5c6a99c77a455e8b
MinnyMind <natapohn.pat@gmail.com>
2025-02-06 16:09:00 +0700

    .

--- lib/pages/playlist.dart
+++ lib/pages/playlist.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Playlist extends StatelessWidget {
+  const Playlist({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
